dc6f29f9f93ace9599089ab7e29e424a6eb7e8d8
Manu <mcabral@frba.utn.edu.ar>
2022-12-08 21:16:44 -0300

    feat: add login routes

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const userSchemas = require('../schemas/user');
+const authController = require('../controllers/auth');
+const schemaValidator = require('../middlewares/schemaValidator');
+
+router.post('/login', schemaValidator(userSchemas.login), authController.login);
+router.post('/register', schemaValidator(userSchemas.create), authController.register);
+
+module.exports = router;
