0289a56fd0d2da79a68d98860aca4931fc5e7ce5
Manu <mcabral@frba.utn.edu.ar>
2022-12-08 21:15:43 -0300

    feat: add login user schema

--- schemas/user.js
+++ schemas/user.js
@@ -6,5 +6,18 @@ module.exports = {
             isLength: { options: { min: 2 }, errorMessage: 'First name must be at least 2 characters long' },
             exists: { errorMessage: 'First name is required' },
         },
+    },
+    login: {
+        email: {
+            in: ['body'],
+            isEmail: { errorMessage: 'Email must be a valid email address' },
+            exists: { errorMessage: 'Email is required' },
+        },
+        password: {
+            in: ['body'],
+            isString: { errorMessage: 'Password must be a string' },
+            isLength: { options: { min: 6 }, errorMessage: 'Password must be at least 6 characters long' },
+            exists: { errorMessage: 'Password is required' },
+        }
     }
 }
