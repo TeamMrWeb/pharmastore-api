3879401b63bb9546c2980e67c2950db4fa9562cc
Manu <mcabral@frba.utn.edu.ar>
2022-12-08 21:17:29 -0300

    feat: add auth route in index routes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 // setting up routes
 router.get('/', (req, res) => res.send('Parches Pharmastore API v1'));
 
+router.use('/auth', require('./auth'));
 router.use('/user', require('./user'));
 
 module.exports = router;
