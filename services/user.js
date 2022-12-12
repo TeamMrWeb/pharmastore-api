ee731ef0677d6cbf234476c18fa831fac1222979
Manu <mcabral@frba.utn.edu.ar>
2022-12-12 18:30:49 -0300

    feat: add user getById and getByEmail

--- services/user.js
+++ services/user.js
@@ -2,5 +2,7 @@ const { User } = require('../database/models');
 
 module.exports = {
     create: async (payload) => await User.create(payload),
-    getAll: async () => await User.findAll()
+    getAll: async () => await User.findAll(),
+    getById: async (id) => await User.findByPk(id),
+    getByEmail: async (email) => await User.findOne({ where: { email } }),
 }
