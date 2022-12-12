0c4e5930a7396776448705014fd810183f4ac9bf
Manu <mcabral@frba.utn.edu.ar>
2022-12-12 18:30:29 -0300

    feat: add auth login route

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const userSchemas = require('../schemas/user');
 const authController = require('../controllers/auth');
 const schemaValidator = require('../middlewares/schemaValidator');
 
-//router.post('/login', schemaValidator(userSchemas.login), authController.login);
+router.post('/login', schemaValidator(userSchemas.login), authController.login);
 router.post('/register', schemaValidator(userSchemas.create), authController.register);
 
 module.exports = router;
