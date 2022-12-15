5f614b75161996af2d444bc9b7e4b63b0691d7b2
Manu <mcabral@frba.utn.edu.ar>
2022-12-14 22:26:18 -0300

    fix: exclude password field on find all

--- services/user.js
+++ services/user.js
@@ -2,7 +2,7 @@ const { User } = require('../database/models');
 
 module.exports = {
     create: async (payload) => await User.create(payload),
-    getAll: async () => await User.findAll(),
+    getAll: async () => await User.findAll({ attributes: { exclude: ['password'] } }),
     getById: async (id) => await User.findByPk(id),
     getByEmail: async (email) => await User.findOne({ where: { email } }),
 }
