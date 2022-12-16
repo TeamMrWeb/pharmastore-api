c32dc6d6581eac14c5e31b19a844075615b257e7
Manu <mcabral@frba.utn.edu.ar>
2022-12-16 18:49:38 -0300

    feat: add product category model

---
+++ database/models/product_category.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class product_category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  product_category.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'product_category',
+  });
+  return product_category;
+};
