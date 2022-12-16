ff5a23114856aade1b23f1d64fa92c8c32068ae9
Manu <mcabral@frba.utn.edu.ar>
2022-12-16 19:22:23 -0300

    fix: category id type

--- database/migrations/1-create-product-category.js
+++ database/migrations/1-create-product-category.js
@@ -7,6 +7,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
+        type: Sequelize.INTEGER
       },
       name: {
         type: Sequelize.STRING
