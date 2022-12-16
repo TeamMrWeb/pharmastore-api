95fbaeb1a82bfaa852562c6ebb01bd94bd1c73b8
Manu <mcabral@frba.utn.edu.ar>
2022-12-16 19:05:35 -0300

    feat: add products seeder

---
+++ database/seeders/5-products.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('products', [
+        {
+            name: 'Shampoo',
+            description: 'Shampoo for hair!!',
+            price: 100,
+            categoryId: 1,
+            inventoryId: 1,
+            // discountId: 1, // NOTE: use the product-discount seeder to make a relationship between products and discounts
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            name: 'Toothbrush',
+            description: 'Toothbrush',
+            price: 100,
+            categoryId: 2,
+            inventoryId: 2,
+            // discountId: 1, // NOTE: use the product-discount seeder to make a relationship between products and discounts
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+    ], {
+        ignoreDuplicates: true,
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('products', null, {});
+  },
+};
