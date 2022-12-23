a3a9a62eee475f4856eb8b99733581d05034541b
Manu <mcabral@frba.utn.edu.ar>
2022-12-22 21:30:43 -0300

    feat: add user pagination

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
-const createHttpError = require('http-errors')
+const createHttpError = require('http-errors');
+const errorObject = require('../helpers/errorObject');
 const catchAsync = require('../helpers/catchAsync');
 const successResponse = require('../helpers/successResponse');
 
@@ -7,11 +8,17 @@ const userService = require('../services/user');
 module.exports = {
     get: catchAsync(async (req, res, next) => {
         try {
-            const results = await userService.getAll();
+            const page = req.query.page || 0;
+            if (page < 0) throw new errorObject({ statusCode: 400, message: 'Page cannot be less than 0' });
+            const results = await userService.get({ page });
             successResponse({
                 res,
                 message: 'Users fetched successfully',
-                body: results
+                body: {
+                    users: results,
+                    previous: page > 0 ? `/v1/user?page=${page - 1}` : null,
+                    next: results.length > 0 ? `/v1/user?page=${parseInt(page)+1}` : null
+                }
             });
         } catch (err) {
             next(createHttpError(err.statusCode,`[Error retrieving users] - [users - GET]: ${err.message}`))
--- services/user.js
+++ services/user.js
@@ -1,6 +1,7 @@
 const { User } = require('../database/models');
 
 module.exports = {
+    get: async({ page = 0, limit = 10}) => await User.findAll({ offset: page * limit, limit, attributes: { exclude: ['password'] } }),
     create: async (payload) => await User.create(payload),
     getAll: async () => await User.findAll({ attributes: { exclude: ['password'] } }),
     getById: async (id) => await User.findByPk(id),
