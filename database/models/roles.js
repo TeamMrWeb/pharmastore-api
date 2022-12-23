f3ea79cb17e47b80fec5ff9c04586d9663959857
Manu <mcabral@frba.utn.edu.ar>
2022-12-22 21:14:57 -0300

    fix: role and user assoc.

--- database/models/roles.js
+++ database/models/roles.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-        Roles.hasOne(models.Users)
+        Roles.hasOne(models.User)
     }
   }
   Roles.init({
