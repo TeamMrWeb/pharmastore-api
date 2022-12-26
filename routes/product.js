e747d959651af290e29e63e22dc67c71b8b4a0e1
Manu <mcabral@frba.utn.edu.ar>
2022-12-26 17:24:52 -0300

    feat: add get products route

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const productController = require('../controllers/product');
 const categoryController = require('../controllers/category');
 
+router.get('/', productController.getProducts);
 router.get('/categories', categoryController.getCategories);
 
 module.exports = router;
