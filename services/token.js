7f5ea5170ad872675c86fd02bfe8eb317ed384c1
Manu <mcabral@frba.utn.edu.ar>
2022-12-25 23:42:16 -0300

    feat: add verify token

--- services/token.js
+++ services/token.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const moment = require('moment');
+const errorObject = require('../helpers/errorObject');
 
 const { Token } = require('../database/models');
 const tokens = require('../config').tokens
@@ -33,6 +34,23 @@ module.exports = {
         });
     },
 
+    verifyToken: async ({ token, type }) => {
+        try {
+            const decoded = jwt.verify(token, tokens.secret);
+            if (decoded.type !== type) throw new Error('Invalid token type');
+            const tokenRecord = await Token.findOne({ where: {
+                token,
+                type,
+                userId: decoded.userId,
+                blacklisted: false
+            }});
+            if (!tokenRecord) throw new Error('Token not found');
+            if (moment().isAfter(moment(tokenRecord.expiresAt))) throw new Error('Token expired');
+            return tokenRecord;
+        } catch (err) {
+            throw new errorObject({ statusCode: 400, message: err.message });
+        }
+    },
 
     generateNeccessaryTokens: async (userId) => {
         const accessTokenEx = moment().add(parseInt(tokens.accessToken.expires), 'minutes')
