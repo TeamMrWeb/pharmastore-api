e63b2243d4ef0811d0724a385dbddd77bacc5c9a
Manu <mcabral@frba.utn.edu.ar>
2022-12-26 15:39:03 -0300

    feat: add product route

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ router.get('/', (req, res) => res.send('Parches Pharmastore API v1'));
 
 router.use('/auth', require('./auth'));
 router.use('/user', require('./user'));
+router.use('/product', require('./product'));
 
 module.exports = router;
