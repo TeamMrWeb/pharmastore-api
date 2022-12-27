1ba6e88d597537e4889800452fe07d6e905da537
Manu <mcabral@frba.utn.edu.ar>
2022-12-27 17:59:57 -0300

    feat: add product relationships on findAll

--- services/product.js
+++ services/product.js
@@ -1,6 +1,21 @@
-const { Product } = require('../database/models');
+const { Product, product_category, product_inventory, product_discount } = require('../database/models');
+const models = require('../database/models');
 
 module.exports = {
-    get: async({ page = 0, limit = 10}) => await Product.findAll({ offset: page * limit, limit }),
+    get: async({ page = 0, limit = 10}) => {
+        return await Product.findAll({
+            include: [
+                { model: product_category, attributes: ['name', 'description'] },
+                { model: product_inventory, attributes: ['quantity'] },
+                {
+                    model: product_discount,
+                    attributes: ['name', 'description', 'percent'],
+                    where: { active: true }
+                }
+            ],
+            offset: page * limit,
+            limit: limit
+        })
+    },
     create: async (payload) => await Product.create(payload),
 }
