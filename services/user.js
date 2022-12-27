ea215dff6e4ff4c20b4810a8603f393630e083c7
Manu <mcabral@frba.utn.edu.ar>
2022-12-27 16:46:15 -0300

    feat: include role relationship on users

--- services/user.js
+++ services/user.js
@@ -1,7 +1,12 @@
-const { User } = require('../database/models');
+const { User, Roles } = require('../database/models');
 
 module.exports = {
-    get: async({ page = 0, limit = 10}) => await User.findAll({ offset: page * limit, limit, attributes: { exclude: ['password'] } }),
+    get: async({ page = 0, limit = 10}) => await User.findAll({
+        offset: page * limit, limit,
+        attributes: { exclude: ['password'] },
+        include: { model: Roles, attributes: ['name'] }
+    }),
+    getOne: async (id) => await User.findByPk(id, { attributes: { exclude: ['password'] }, include: { model: Roles, attributes: ['name'] }}),
     create: async (payload) => await User.create(payload),
     getAll: async () => await User.findAll({ attributes: { exclude: ['password'] } }),
     getById: async (id) => await User.findByPk(id),
