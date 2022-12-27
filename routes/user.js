0605d0e3e3619541986753e25b4eb3ac00fc2966
Manu <mcabral@frba.utn.edu.ar>
2022-12-27 16:46:05 -0300

    feat: add get user by id route

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../controllers/user');
 
 router.get('/', userController.get)
+router.get('/:id', userController.getOne)
 
 module.exports = router;
