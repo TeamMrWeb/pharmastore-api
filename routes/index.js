2d481f444c4a1c9734f9fd54393943f282549b2b
FedeGauna16 <fede.face.gauna@gmail.com>
2022-12-28 21:03:32 -0300

    feat: add rol routes

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,6 @@ router.get('/', (req, res) => res.send('Parches Pharmastore API v1'));
 router.use('/auth', require('./auth'));
 router.use('/user', require('./user'));
 router.use('/product', require('./product'));
+router.use('/rol', require('./rol'));
 
 module.exports = router;
