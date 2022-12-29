31364348f651c7349cc55f0e269b456c50fe13b9
FedeGauna16 <fede.face.gauna@gmail.com>
2022-12-29 14:42:52 -0300

    refractor: change variable name in category service

--- services/category.js
+++ services/category.js
@@ -2,4 +2,5 @@ const { product_category }= require('../database/models');
 
 module.exports = {
     getAll: async () => await product_category.findAll(),
+    findCategoryById: async (id) => await product_category.findByPk(id)
 }
