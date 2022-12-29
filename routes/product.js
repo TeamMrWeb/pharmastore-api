2fb7d2adcd39afb1b5c47baa479345678c03e5f2
FedeGauna16 <fede.face.gauna@gmail.com>
2022-12-28 22:51:25 -0300

    feat: add POST product route

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const productSchemas = require('../schemas/product');
+
 const productController = require('../controllers/product');
 const categoryController = require('../controllers/category');
+const schemaValidator = require('../middlewares/schemaValidator');
 
 router.get('/', productController.getProducts);
 router.get('/categories', categoryController.getCategories);
+router.post('/', schemaValidator(productSchemas.create), productController.postProducts);
+
 
 module.exports = router;
