4390ee85cd03c0fe56964c6e7b86a206a3861a05
FedeGauna16 <fede.face.gauna@gmail.com>
2022-12-30 17:21:33 -0300

    feat: add route on user routes

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const userController = require('../controllers/user');
 
 router.get('/', userController.get)
+router.get('/filterByName', userController.getFilterByName)
 router.get('/:id', userController.getOne)
+
 
 module.exports = router;
