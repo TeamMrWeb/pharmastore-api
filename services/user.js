6c1e2294bfe13b128bc65076ce7f07f56d9c78a1
FedeGauna16 <fede.face.gauna@gmail.com>
2022-12-30 17:23:43 -0300

    feat: add new function on user services

--- services/user.js
+++ services/user.js
@@ -1,4 +1,5 @@
 const { User, Roles } = require('../database/models');
+const { Op } = require('sequelize');
 
 module.exports = {
     get: async({ page = 0, limit = 10}) => await User.findAll({
@@ -11,4 +12,13 @@ module.exports = {
     getAll: async () => await User.findAll({ attributes: { exclude: ['password'] } }),
     getById: async (id) => await User.findByPk(id),
     getByEmail: async (email, secure=True) => await User.findOne({ where: { email }, attributes: secure? { exclude: ['password'] } : null }),
+    getByName: async({ page = 0, limit = 10, firstName, lastName }) => await User.findAll({
+            attributes: { exclude: ['password'] },
+            where: { [Op.or]: 
+                [{firstName: { [Op.like]: `%${firstName}%` }},
+                {lastName: { [Op.like]: `%${lastName}%` }}]
+            },
+            offset: page * limit,
+            limit: limit
+        })
 }
