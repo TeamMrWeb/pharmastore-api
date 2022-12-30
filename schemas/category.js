0344985666b6dcf6dd2c1c33d051c6a52ba33900
Manu <mcabral@frba.utn.edu.ar>
2022-12-29 22:46:04 -0300

    feat: add category create schema

---
+++ schemas/category.js
@@ -0,0 +1,16 @@
+module.exports = {
+    create: {
+        name: {
+            in: ['body'],
+            isString: { errorMessage: 'Name must be a string' },
+            isLength: { options: { min: 2 }, errorMessage: 'Name must be at least 2 characters long' },
+            exists: { errorMessage: 'Name is required' },
+        },
+        description: {
+            in: ['body'],
+            isString: { errorMessage: 'Description must be a string' },
+            isLength: { options: { min: 2 }, errorMessage: 'Description must be at least 2 characters long' },
+            exists: { errorMessage: 'Description is required' },
+        }
+    },
+}
