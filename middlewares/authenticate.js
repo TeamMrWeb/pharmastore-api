518221f6067ea87edb1cf03576ef8df079bde0f5
Manu <mcabral@frba.utn.edu.ar>
2022-12-31 09:34:02 -0300

    fix(middlewares): remove lines

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,10 +9,9 @@ module.exports = {
         async(req, res, next) => {
             try {
                 // TODO: verification and validation
-                const token = req.headers.authorization.split(' ')[1];
-                console.log(token)
                 next();
             } catch (err) {
+                console.log(err)
                 next(createHttpError(err.statusCode,`[Error on authenticate] ${err.message}`));
             }
         }
--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -5,16 +5,14 @@ const catchAsync = require('../helpers/catchAsync');
 // const { ROLES } = require('../constants');
 
 module.exports = {
-    authenticate: catchAsync(
+    isAdmin: catchAsync(
         async(req, res, next) => {
             try {
                 // TODO: verify if user is admin
-                const user = req.user;
-                if (!user)
-                    throw new errorObject(401, 'Unauthorized');
                 next();
             } catch (err) {
-                next(createHttpError(err.statusCode,`[Error on authenticate] ${err.message}`));
+                console.log(err)
+                next(createHttpError(err.statusCode,`[Error on user authenticate] ${err.message}`));
             }
         }
     )
