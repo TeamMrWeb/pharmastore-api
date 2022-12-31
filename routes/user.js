0628151b22b19fac82caf332b8dac98c7a65cb2c
Manu <mcabral@frba.utn.edu.ar>
2022-12-31 09:16:09 -0300

    feat: add put and patch route user methods

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,7 @@ const userController = require('../controllers/user');
 router.get('/', userController.get)
 router.get('/:id', userController.getOne)
 
+router.put('/:id', userController.update)
+router.patch('/:id', userController.update)
 
 module.exports = router;
