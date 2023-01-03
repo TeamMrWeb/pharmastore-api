cd393eadd705fc00ec176a6b53e01f28a88cc19c
FedeGauna16 <fede.face.gauna@gmail.com>
2023-01-03 14:06:23 -0300

    feat: own authenticator
    Co-authored-by: Manuel Cabral <mandrescabral@gmail.com>

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -5,25 +5,25 @@ const catchAsync = require('../helpers/catchAsync');
 const { ROLES } = require('../constants');
 const userService = require('../services/user');
 
-
 module.exports = {
     isAdmin: catchAsync(
         async(req, res, next) => {
             try {
-                // TODO: verify if user is admin
                 const payload = req.payload;
-                if(!payload) throw new Error('Not authorizated');
-                //console.log(payload);
+                if (!payload) throw new Error('Not authorizated');
                 const user = await userService.getById(payload.userId);
-                //console.log(user);
-                if(!user) throw new Error('User doesnt exists');
+                if (!user) throw new Error('User doesnt exists');
                 const rolUser = user.roleId;
-                console.log(rolUser);
-                if(rolUser !== ROLES.ADMIN) throw new Error('Not authorizated');
-                next();
+                const { id } = req.params;
+                const targetId = parseInt(id);
+                if (targetId === user.id)
+                    return next();
+                if(rolUser === ROLES.ADMIN)
+                    return next();
+                throw new Error('Not authorizated')
             } catch (err) {
                 console.log(err)
-                next(createHttpError(err.statusCode,`[Error on user authenticate] ${err.message}`));
+                next(createHttpError(401,`[Error on user authenticate] ${err.message}`));
             }
         }
     )
