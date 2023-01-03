ee57b52a0d1d54771d608fcfd8335cbe6668a22f
FedeGauna16 <fede.face.gauna@gmail.com>
2023-01-02 22:33:10 -0300

    feat: add isAdmin logic middleware

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -2,13 +2,24 @@ const createHttpError = require('http-errors');
 const errorObject = require('../helpers/errorObject');
 const catchAsync = require('../helpers/catchAsync');
 
-// const { ROLES } = require('../constants');
+const { ROLES } = require('../constants');
+const userService = require('../services/user');
+
 
 module.exports = {
     isAdmin: catchAsync(
         async(req, res, next) => {
             try {
                 // TODO: verify if user is admin
+                const payload = req.payload;
+                if(!payload) throw new Error('Not authorizated');
+                //console.log(payload);
+                const user = await userService.getById(payload.userId);
+                //console.log(user);
+                if(!user) throw new Error('User doesnt exists');
+                const rolUser = user.roleId;
+                console.log(rolUser);
+                if(rolUser !== ROLES.ADMIN) throw new Error('Not authorizated');
                 next();
             } catch (err) {
                 console.log(err)
