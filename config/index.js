f66b04cbe8ad5135ec3bf6e482f9409824c5638d
Manu <mcabral@frba.utn.edu.ar>
2023-01-04 17:03:46 -0300

    feat: add test dev config

--- config/index.js
+++ config/index.js
@@ -36,5 +36,15 @@ module.exports = {
         dialect: process.env.DATABASE_DIALECT,
         jwtSecret: process.env.JWT_SECRET,
         jwtExpire: process.env.JWT_EXPIRE
+    },
+    test: {
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        dialect: process.env.DATABASE_DIALECT,
+        jwtSecret: process.env.JWT_SECRET,
+        jwtExpire: process.env.JWT_EXPIRE
     }
 }
