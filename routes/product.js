bfe77e6dc79fa6637584e1befbb3f55b5c1a3448
Manu <mcabral@frba.utn.edu.ar>
2023-01-04 00:14:35 -0300

    feat(routes): add authenticate middleware

--- routes/product.js
+++ routes/product.js
@@ -11,10 +11,10 @@ const schemaValidator = require('../middlewares/schemaValidator');
 const { authenticate } = require('../middlewares/authenticate');
 const { isAdmin } = require('../middlewares/isAdmin');
 
-router.get('/', productController.getProducts);
+router.get('/', authenticate, productController.getProducts);
 router.post('/', schemaValidator(productSchemas.create), authenticate, isAdmin, productController.postProducts);
 
-router.get('/categories', categoryController.getCategories);
+router.get('/categories', authenticate, categoryController.getCategories);
 router.post('/categories', schemaValidator(categorySchema.create), authenticate, isAdmin, categoryController.createCategory);
 
 
--- routes/user.js
+++ routes/user.js
@@ -9,8 +9,8 @@ const schemaValidator = require('../middlewares/schemaValidator');
 const { authenticate } = require('../middlewares/authenticate');
 const { isAdmin } = require('../middlewares/isAdmin');
 
-router.get('/', userController.get)
-router.get('/:id', userController.getOne)
+router.get('/', authenticate, userController.get)
+router.get('/:id', authenticate, userController.getOne)
 
 router.put('/:id', schemaValidator(userSchema.update), authenticate, isAdmin, userController.update)
 router.patch('/:id', authenticate, isAdmin, userController.update)
