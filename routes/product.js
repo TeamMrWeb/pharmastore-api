c95c8f80d3a60bd392b071e183888d7251a0226f
FedeGauna16 <fede.face.gauna@gmail.com>
2023-01-04 16:52:37 -0300

    feat: add put and patch product routes

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const productSchemas = require('../schemas/product');
+const productSchema = require('../schemas/product');
 const categorySchema = require('../schemas/category');
 
 const productController = require('../controllers/product');
@@ -12,10 +12,15 @@ const { authenticate } = require('../middlewares/authenticate');
 const { isAdmin } = require('../middlewares/isAdmin');
 
 router.get('/', authenticate, productController.getProducts);
-router.post('/', schemaValidator(productSchemas.create), authenticate, isAdmin, productController.postProducts);
+router.post('/', schemaValidator(productSchema.create), authenticate, isAdmin, productController.postProducts);
+router.put('/:id', schemaValidator(productSchema.update), authenticate, isAdmin, productController.update);
+router.patch('/:id', authenticate, isAdmin, productController.update);
+
 
 router.get('/categories', authenticate, categoryController.getCategories);
 router.post('/categories', schemaValidator(categorySchema.create), authenticate, isAdmin, categoryController.createCategory);
+router.put('/categories/:id', schemaValidator(categorySchema.update), authenticate, isAdmin, categoryController.update);
+router.patch('/categories/:id', authenticate, isAdmin, categoryController.update);
 
 
 module.exports = router;
