8cd8e15cd366dbdb178f2b175fc8d1dc2105033d
FedeGauna16 <fede.face.gauna@gmail.com>
2023-01-04 16:52:06 -0300

    feat: add update on category and product controllers

--- controllers/category.js
+++ controllers/category.js
@@ -32,5 +32,36 @@ module.exports = {
             console.log(err)
             next(createHttpError(err.statusCode,`[Error creating categories] - [categories - POST]: ${err.message}`))
         }
+    }),
+    update: catchAsync(async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            if (!id) throw new errorObject({ statusCode: 400, message: 'Category ID is required' });
+            let payload = {};
+            const method = req.method
+            if (method === 'PUT')
+                payload = req.body;
+            else
+            {
+                const attributes = await categoryService.getAttributes();
+                attributes.forEach(attr => {
+                // If any attr is provided, update it
+                if (method === 'PATCH' && req.body[attr])
+                    payload[attr] = req.body[attr]; 
+                })
+            }
+            if (Object.keys(payload).length === 0) throw new errorObject({ statusCode: 400, message: 'No valid attributes provided' });
+            console.log(payload)
+            const category = await categoryService.update(id, payload);
+            if (!category) throw new errorObject({ statusCode: 404, message: 'Category not found' });
+            successResponse({
+                res,
+                message: 'Category updated successfully',
+                body: category
+            });
+        } catch (err) {
+            console.log(err)
+            next(createHttpError(err.statusCode,`[Error updating category] - [category - ${req.method}]: ${err.message}`))
+        }
     })
 }
