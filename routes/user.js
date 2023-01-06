c9dd4c7b0fc83c1c31c5289f88838a14231787a1
Manu <mcabral@frba.utn.edu.ar>
2023-01-06 16:52:15 -0300

    feat(routes): add user payment routes

--- routes/user.js
+++ routes/user.js
@@ -9,6 +9,12 @@ const schemaValidator = require('../middlewares/schemaValidator');
 const { authenticate } = require('../middlewares/authenticate');
 const { isAdmin } = require('../middlewares/isAdmin');
 
+// TODO: payment routes, some routes require a schema validator
+router.get('/payment', authenticate, userController.getPayment)
+router.post('/payment', (req, res) => { res.send('Payment post route') } )
+router.put('/payment', (req, res) => { res.send('Payment update route') } ) 
+router.patch('/payment', (req, res) => { res.send('Payment patch route') } )
+
 router.get('/', authenticate, userController.get)
 router.get('/:id', authenticate, userController.getOne)
 
@@ -17,5 +23,7 @@ router.patch('/:id', authenticate, isAdmin, userController.update)
 
 // NOTE: remove foreign key constraints before deleting a user
 router.delete('/:id', authenticate, isAdmin, userController.delete)
+
+
 
 module.exports = router;
