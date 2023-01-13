13b44fc270a7430036827aae2e1ba8441e30c296
Manu <mcabral@frba.utn.edu.ar>
2023-01-13 11:15:30 -0300

    feat(service): add product get one

--- services/product.js
+++ services/product.js
@@ -3,56 +3,67 @@ const {
     product_category,
     product_inventory,
     product_discount,
-} = require('../database/models');
+} = require('../database/models')
 
-const { Op } = require('sequelize');
-const errorObject = require('../helpers/errorObject');
+const { Op } = require('sequelize')
+const errorObject = require('../helpers/errorObject')
 
-const categoryService = require('./category');
-const inventoryService = require('./inventory');
-const discountService = require('./discount');
+const categoryService = require('./category')
+const inventoryService = require('./inventory')
+const discountService = require('./discount')
 
 module.exports = {
-    get: async({ page = 0, limit = 10, category, rating, name }) => {
+    get: async ({ page = 0, limit = 10, category, rating, name }) => {
         return await Product.findAll({
             where: {
                 name: name ? { [Op.like]: `%${name}%` } : { [Op.ne]: null },
-                rating: rating ? { [Op.gte]: rating } : { [Op.ne]: null }
+                rating: rating ? { [Op.gte]: rating } : { [Op.ne]: null },
             },
             include: [
                 {
-                    model: product_category, attributes: ['name', 'description'],
-                    where: category ? { name: { [Op.like]: `%${category}%` } } : {}
+                    model: product_category,
+                    attributes: ['name', 'description'],
+                    where: category
+                        ? { name: { [Op.like]: `%${category}%` } }
+                        : {},
                 },
                 {
                     model: product_inventory,
                     attributes: ['quantity'],
-                    where: { quantity: { [Op.gt]: 0 } }
+                    where: { quantity: { [Op.gt]: 0 } },
                 },
                 {
                     model: product_discount,
                     attributes: ['name', 'description', 'percent'],
-                    where: { active: true }
+                    where: { active: true },
                 },
-                'images'
+                'images',
             ],
             offset: page * limit,
-            limit: limit
+            limit: limit,
         })
     },
     create: async (payload) => await Product.create(payload),
     validateProduct: async (payload) => {
-        try{
-            const categoryId = await categoryService.findCategoryById(payload.categoryId);
-            if(!categoryId) throw new Error('CategoryId doesnt exists');
-            const inventoryId = await inventoryService.findInventoryById(payload.inventoryId);
-            if(!inventoryId) throw new Error('InventoryId doesnt exists');
-            const discountId = await discountService.findDiscountById(payload.discountId);
-            if(!discountId) throw new Error('DiscountId doesnt exists');
-                } catch(err) {
-            throw new errorObject({ statusCode: 404, message: err.message });
+        try {
+            const categoryId = await categoryService.findCategoryById(
+                payload.categoryId
+            )
+            if (!categoryId) throw new Error('CategoryId doesnt exists')
+            const inventoryId = await inventoryService.findInventoryById(
+                payload.inventoryId
+            )
+            if (!inventoryId) throw new Error('InventoryId doesnt exists')
+            const discountId = await discountService.findDiscountById(
+                payload.discountId
+            )
+            if (!discountId) throw new Error('DiscountId doesnt exists')
+        } catch (err) {
+            throw new errorObject({ statusCode: 404, message: err.message })
         }
     },
     getAttributes: async () => await Object.keys(Product.rawAttributes),
-    update: async (id, payload) => await Product.update(payload, { where: { id } })
+    update: async (id, payload) =>
+        await Product.update(payload, { where: { id } }),
+    getOne: async (id) => await Product.findByPk(id),
 }
