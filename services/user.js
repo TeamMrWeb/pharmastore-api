f711c6637fef74864941fc1c555c17f53cafb929
Manu <mcabral@frba.utn.edu.ar>
2023-01-13 11:09:47 -0300

    style: format and remove console log

--- services/user.js
+++ services/user.js
@@ -1,17 +1,16 @@
-const { User, Roles, user_payment } = require('../database/models');
-const { Op } = require('sequelize');
-
-console.log(user_payment, Roles)
+const { User, Roles, user_payment } = require('../database/models')
+const { Op } = require('sequelize')
 
 module.exports = {
-    get: async({
-            page = 0,
-            limit = 10,
-            firstName,
-            lastName,
-            deleted = false,
-            role,
-        }) => await User.findAll({
+    get: async ({
+        page = 0,
+        limit = 10,
+        firstName,
+        lastName,
+        deleted = false,
+        role,
+    }) =>
+        await User.findAll({
             limit,
             offset: page * limit,
             attributes: { exclude: ['password'] },
@@ -19,26 +18,42 @@ module.exports = {
                 {
                     model: Roles,
                     attributes: ['name'],
-                    where: { name: { [Op.like]: `%${role}%`} }
+                    where: { name: { [Op.like]: `%${role}%` } },
                 },
                 {
                     model: user_payment,
-                    attributes: ['payment_type', 'provider', 'account_no', 'expiry']
-                }
+                    attributes: [
+                        'payment_type',
+                        'provider',
+                        'account_no',
+                        'expiry',
+                    ],
+                },
             ],
             where: {
                 firstName: { [Op.like]: `%${firstName}%` },
-                lastName: { [Op.like]: `%${lastName}%` }
+                lastName: { [Op.like]: `%${lastName}%` },
             },
-            paranoid: !deleted
+            paranoid: !deleted,
         }),
     getAttributes: async () => await Object.keys(User.rawAttributes),
-    update: async (id, payload) => await User.update(payload, { where: { id } }),
-    getOne: async (id) => await User.findByPk(id, { attributes: { exclude: ['password'] }, include: { model: Roles, attributes: ['name'] }}),
-    getPayment: async (id) => await user_payment.findOne({ where: { userId: id } }),
+    update: async (id, payload) =>
+        await User.update(payload, { where: { id } }),
+    getOne: async (id) =>
+        await User.findByPk(id, {
+            attributes: { exclude: ['password'] },
+            include: { model: Roles, attributes: ['name'] },
+        }),
+    getPayment: async (id) =>
+        await user_payment.findOne({ where: { userId: id } }),
     create: async (payload) => await User.create(payload),
-    getAll: async () => await User.findAll({ attributes: { exclude: ['password'] } }),
+    getAll: async () =>
+        await User.findAll({ attributes: { exclude: ['password'] } }),
     getById: async (id) => await User.findByPk(id),
-    getByEmail: async (email, secure=True) => await User.findOne({ where: { email }, attributes: secure? { exclude: ['password'] } : null }),
+    getByEmail: async (email, secure = True) =>
+        await User.findOne({
+            where: { email },
+            attributes: secure ? { exclude: ['password'] } : null,
+        }),
     delete: async (id) => await User.destroy({ where: { id } }),
 }
